3f0bba0bd62f42ed318d30bdd66bdea6d4a69cbd
venkateshramkumarg <venkateshramkumar3@gmail.com>
2024-09-10 17:50:12 +0530

    Update CORS origin to allow all origins

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import commentsGroup from './api/comments'
 const app = new Hono().basePath('/api')
 
 app.use('*', cors({
-  origin: ['http://localhost:3001'], 
+  origin: ['*'], 
   allowMethods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowHeaders: ['Content-Type', 'Authorization'],
 }));
